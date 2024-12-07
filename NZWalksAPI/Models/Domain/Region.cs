b084a4629100b42450cd3591dab56f15670caa89
benito JD <benito.josede@gmail.com>
2024-12-07 19:27:23 +0530

    added few classes

---
+++ NZWalksAPI/Models/Domain/Region.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NZWalksAPI.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public required string Code { get; set; }
+        public required string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+
+    }
+}
---
+++ NZWalksAPI/Models/Domain/Walk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NZWalksAPI.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public double WalkImageUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+        
+        // Navigation Property
+        public required Difficulty Difficulty { get; set; }
+        public required Region Region { get; set; }
+
+    }
+}
